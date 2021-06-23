d40a18d443143d53e0afca82397d0f56c21e236b
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-06-23 16:03:31 +0200

    add task

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@ Descrizione: Rifare l'esercizio della to do list come fatto assieme in classe:
 - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
 - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista 
 */
+
+const app = new Vue({
+    el: "#app",
+    data: {
+        newTask: "",
+        tasks: [
+            "Fare la spesa",
+            "Pulire casa",
+            "Comprare cibo gatti",
+            "Chiamare Mike"
+        ]
+    },
+    methods: {
+        addTask(){
+            this.tasks.push(this.newTask);
+            this.newTask = "";
+        }
+    }
+});
